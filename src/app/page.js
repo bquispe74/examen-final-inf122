057db524d1f1b1baf53c0f778b2bb27d2b3b0839
bquispe74 <brayan.lt.80@gmai.com>
2024-01-26 12:04:14 -0400

    comit

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
-import Image from "next/image";
+//import Image from "next/image";
 import styles from "./page.module.css";
-import Pokedex from "./componentes/pokemon";
+import Pokedex from "./components/pokemon";
 export default function Home() {
   return (
     <main className={styles.main}>
